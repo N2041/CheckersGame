be4e6a465b3a94bbfc040c0e9abc6468119b5022
Nehal <62080303+N2041@users.noreply.github.com>
2023-12-20 11:07:52 -0600

    Testing perhapsss

--- src/checkers/Runner.java
+++ src/checkers/Runner.java
@@ -3,7 +3,7 @@ package checkers;
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Github testing"); //work plzzz
+		System.out.println("Github testing"); //Test perhaps??
 
 	}
 
