a2d412fbffdf37758ca1791f6469c612ada0892f
Nehal <62080303+N2041@users.noreply.github.com>
2023-12-20 10:59:20 -0600

    plz

--- src/checkers/Runner.java
+++ src/checkers/Runner.java
@@ -3,7 +3,7 @@ package checkers;
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Github testing"); //work plz
+		System.out.println("Github testing"); //work plzzz
 
 	}
 
